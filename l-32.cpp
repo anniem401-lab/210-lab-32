10ea8bee07e46cc20aa79c20a8db3f04243bcbc4
Annie <annmora21@gmail.com>
2025-11-11 17:50:47 -0800

    10 min update

--- l-32.cpp
+++ l-32.cpp
@@ -1,2 +1,20 @@
 // COMSC-210 | Lab 32 | Annie Morales
-// Ide used: Visual Studio Code
+// IDE used: Visual Studio Code
+
+// Headers
+#include "Car.h" // To use Car.h file
+#include <deque> // Requirement 1 & for use of std::deque
+using namespace std;
+
+// Constant
+const int In_sz = 2; // Initial size of deque
+
+int main(){
+    // main code
+    deque<Car> cars;
+    cars.push_back(Car());
+
+
+    void print(); // .print() method from Car class
+    return 0;
+}
