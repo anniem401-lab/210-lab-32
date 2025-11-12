ae37bbdb76abdf734931ebb616301234843f55c4
Annie <annmora21@gmail.com>
2025-11-12 14:20:10 -0800

    10 min update

--- l-32.cpp
+++ l-32.cpp
@@ -28,8 +28,24 @@ int main(){
     while(cars.size() > 1){
     int i = 1;
     cout << "Time: " << i << " Operation: "; // Car joins or Car pays and leaves
-    
+    // 55% probability that the car at the head of the line pays its toll and leaves the toll booth
+    // 45% probability that another car joins the line for the toll booth
     }
     */
+
+    for (int i = 1; i = cars.size(); i++){
+        cout << "Time: " << i << " Operation: ";
+        int prob = rand() % 100 + 1;
+        if (prob <= 55) {// 55% probability that the car at the head of the line pays its toll and leaves the toll booth
+            cars.pop_front(); // Removes car at head in the deque
+        }
+        
+        prob = rand() % 100 + 1;
+        if(prob <= 45) {// 45% probability that another car joins the line for the toll booth
+            //string name = cars(Cars);
+            cars.push_back(Car()); // Adds a car to back of the deque
+        }
+    }
+
     return 0;
 }
