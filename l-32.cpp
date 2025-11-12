bdec91d3b4395fc7eaaacbed08de377116a98961
Annie <annmora21@gmail.com>
2025-11-11 19:28:14 -0800

    10 min update

--- l-32.cpp
+++ l-32.cpp
@@ -4,17 +4,31 @@
 // Headers
 #include "Car.h" // To use Car.h file
 #include <deque> // Requirement 1 & for use of std::deque
+#include <ctime>
+#include <cstdlib>
 using namespace std;
 
 // Constant
 const int In_sz = 2; // Initial size of deque
 
 int main(){
-    // main code
+    srand(time(0)); // For random number generation
+
     deque<Car> cars;
+    for(int i = 0; i < In_sz; i++){
     cars.push_back(Car());
+    }
+    cout << "\nInitial queue:" << endl;
+    for(int i = 0; i < cars.size(); i++){
+        cars[i].print(); // Uses print function from Car class
+    }
 
+    // Running cycles
+    while(cars.size() > 1){
+    int i = 1;
+    cout << "Time: " << i << " Operation: "; // Car joins or Car pays and leaves
+    
+    }
 
-    void print(); // .print() method from Car class
     return 0;
 }
