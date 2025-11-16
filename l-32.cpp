ad5b8e8ab3c7908da1a6d3a7217f0e712a171e3e
Annie <annmora21@gmail.com>
2025-11-16 11:06:45 -0800

    10 min update

--- l-32.cpp
+++ l-32.cpp
@@ -15,6 +15,7 @@
 using namespace std;
 
 // Constants
+const int Indent = 6; // for Setw()
 const int Initial_Cars = 2, Car_Count = 8, Num_Lanes = 4, Period_Max = 20;
 const int P_Car_Pays = 46, P_Car_Joins = 39, P_Rear_Change = 15; // Probabilities
 
@@ -28,7 +29,7 @@ int main(){
         cars.push_back(Car()); // Cars pushed into cars deque
     }
     
-    cout << "\nCars amount: " << cars.size() << endl; // Checking amount of cars
+    //cout << "\nCars amount: " << cars.size() << endl; // Checking amount of cars
     for(int i = 0; i < 8; i++){
         cars[i].print(); // Prints cars from cars deque
     }
@@ -45,27 +46,31 @@ int main(){
         cout << "Lane: " << i + 1 << endl;
     
         for(int j = 0; j < Initial_Cars; j++){
-            lanes[i][j].print(); // Prints cars from lanes deque
+            cout << setw(Indent); lanes[i][j].print(); // Prints cars from lanes deque
         }
         
     } cout << endl;
-    cout << "\nCars amount: " << cars.size() << endl; // Checking amount of cars
+    //cout << "\nCars amount: " << cars.size() << endl; // Checking amount of cars
 
     // Running Time Periods
-    int period = 1; // Starts time period at 1, will end at 20
-    cout << "Time: " << period << endl;
-    int prob = rand() % 100 + 1;
-        if (prob <= P_Car_Pays) {// 46% probability that the car at the head of the line pays its toll and leaves the queue
-            int i = 0;
-            cout << "Paid: "; lanes[i].front().print(); // Car at head is printed 
-            lanes[i].pop_front(); // Removes car at head in the deque
+    int i, period = 1; // Starts time period at 1, will end at 20
+    
+    while(!lanes[i].empty()){
+        cout << "Time: " << period << endl;
+        int prob = rand() % 100 + 1;
+        if (prob <= P_Car_Pays){// 46% probability that the car at the head of the line pays its toll and leaves the queue
+            for (int i = 0; i < Num_Lanes; i++){
+                cout << "Lane: " << i + 1 << " Paid: "; lanes[i].front().print(); // Car at head is printed 
+                lanes[i].pop_front(); // Removes car at head in the deque
+            }
         }
 
         prob = rand() % 100 + 1;
-        if (prob <= P_Car_Joins) {// 39% probability that another car joins the queue
-            int i = 0;
-            lanes[i].push_back(Car()); // Adds a car to back of the deque
-            cout << "Joined: "; lanes[i].back().print(); // Car in back is printed
+        if (prob <= P_Car_Joins){// 39% probability that another car joins the queue
+            for (int i = 0; i < Num_Lanes; i++){
+                lanes[i].push_back(Car()); // Adds a car to back of the deque
+                cout << "Lane: " << i + 1 << " Joined: "; lanes[i].back().print(); // Car in back is printed
+            }
         }
 
         cout << endl; // Queue is printed out
@@ -73,10 +78,17 @@ int main(){
         cout << "Lane: " << i + 1 << " Queue:" << endl;
     
         for(int j = 0; j < lanes[i].size(); j++){
-            lanes[i][j].print(); // Prints cars from lanes deque
+            cout << setw(Indent); lanes[i][j].print(); // Prints cars from lanes deque
         }
         
-    } cout << endl;
+        } cout << endl;
+
+        if (lanes[i].empty()){
+            cout << setw(14) << "Empty...\n" << endl;
+        }
+        period++; // increases operation by one
+    }
+
     /*
     while(!cars.empty() && period != 20){
         cout << "Time: " << period << endl;
