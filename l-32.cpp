28ab2f7cebff7f6090405d7e494c5040d53f0017
Annie <annmora21@gmail.com>
2025-11-16 10:09:08 -0800

    10 min update

--- l-32.cpp
+++ l-32.cpp
@@ -33,7 +33,7 @@ int main(){
         cars[i].print(); // Prints cars from cars deque
     }
     
-    // Distribute 2 cars into each lane
+    // Pushes cars unto each lane
     for(int i = 0; i < Num_Lanes; i++){
         lanes[i].push_back(cars[i]);
         lanes[i].push_back(cars[i + 4]);
@@ -56,20 +56,23 @@ int main(){
     cout << "Time: " << period << endl;
     int prob = rand() % 100 + 1;
         if (prob <= P_Car_Pays) {// 46% probability that the car at the head of the line pays its toll and leaves the queue
-            cout << "Car paid: "; cars.front().print(); // Car at head is printed 
-            cars.pop_front(); // Removes car at head in the deque
+            int i = 0;
+            cout << "Paid: "; lanes[i].front().print(); // Car at head is printed 
+            lanes[i].pop_front(); // Removes car at head in the deque
         }
 
+        prob = rand() % 100 + 1;
         if (prob <= P_Car_Joins) {// 39% probability that another car joins the queue
-            cars.push_back(Car()); // Adds a car to back of the deque
-            cout << "Joined lane: "; cars.back().print(); // Car in back is printed
+            int i = 0;
+            lanes[i].push_back(Car()); // Adds a car to back of the deque
+            cout << "Joined: "; lanes[i].back().print(); // Car in back is printed
         }
 
         cout << endl; // Queue is printed out
         for(int i = 0; i < Num_Lanes; i++){
         cout << "Lane: " << i + 1 << " Queue:" << endl;
     
-        for(int j = 0; j < Initial_Cars; j++){
+        for(int j = 0; j < lanes[i].size(); j++){
             lanes[i][j].print(); // Prints cars from lanes deque
         }
         
